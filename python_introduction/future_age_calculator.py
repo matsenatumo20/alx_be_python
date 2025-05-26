36309a962af643e5bee6dd19bdf1cf021df6e20c
Tumo Matsena <matsenatumo20@gmail.com>
2025-05-26 21:12:05 +0200

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,1 +1,7 @@
+user = int(input("How old are you: "))
+current_year = 2023
+estimated_year = 2050
 
+your_age = estimated_year - current_year + user
+
+print(f"In 2050, you will be {your_age} years old.")
