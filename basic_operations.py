62bc23f682a7f0a044917ce1a3bc54f2e35226a8
Tumo Matsena <matsenatumo20@gmail.com>
2025-05-26 16:33:23 +0200

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,4 +1,6 @@
 number1 = 10
 number2 = 5
 
-print(number1 + number2), print(number1 - number2), print(number1 * number2)
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
