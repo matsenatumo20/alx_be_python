0f193d513a7f00757852ef897877042dd996e29f
Tumo Matsena <matsenatumo20@gmail.com>
2025-05-26 17:05:35 +0200

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,10 @@
+number1 = 10
+number2 = 5
 
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+print("Sum of", number1, "and", number2, "is", sum_result)
+print("Difference of", number1, "and", number2, "is", difference_result)
+print("Product of", number1, "and", number2, "is", product_result)
