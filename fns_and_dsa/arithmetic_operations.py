f2dc871699deb1669975a8b37af0f460c8b1f987
Tumo Matsena <matsenatumo20@gmail.com>
2025-06-14 01:00:08 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,17 @@
+def perform_operation(num1: float, num2: float, operation: str) -> float:
 
+    
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 != 0:
+                return "Error, cannot divide by zero"
+            else:
+                return num1 / num2
+        case _:
+            return "Error: Invalid operation. Supported operations are 'add', 'subtract', 'multiply', and 'divide'."
