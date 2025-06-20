257cc0f1f4cf1407d0e6080e2266dfed8bc93428
Tumo Matsena <matsenatumo20@gmail.com>
2025-06-20 22:04:39 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,1 +1,19 @@
+class BankAccount:
+    def __init__(self, initial_balance: float = 0.0):
+        self.__account_balance = initial_balance
 
+    def deposit(self, amount: float):
+        if ammont < 0:
+            raise ValueError("Deposit amount cannot be negative")
+        self.__account_balance += amount
+
+    def withdraw(self, amount: float):
+        if amount < 0:
+            raise ValueError("Withdrawal cannot be negative")
+        if amount > self.__account_balance:
+            return False
+        self.__account_balance -= amount
+        return True
+    
+    def display_balance(self):
+        print(f"Current Balance: {self.__account_balance:.2f}")
